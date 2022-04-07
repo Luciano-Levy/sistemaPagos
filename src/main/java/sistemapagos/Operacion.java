168c153ea8c067846f499a79673302906ca48bed
Luciano Levy <lucianolevy03@gmail.com>
2022-04-06 22:17:27 -0300

    Operacion class

--- src/main/java/sistemapagos/Operacion.java
+++ src/main/java/sistemapagos/Operacion.java
@@ -1,5 +1,27 @@
 package sistemapagos;
 
+import sistemapagos.tarjeta.Tarjeta;
+
 public class Operacion {
+    float valor;
+    Tarjeta tarjeta;
+
+
+    public Operacion(float valor, Tarjeta tarjeta) {
+        this.valor = valor;
+        this.tarjeta = tarjeta;
+    }
     
+    public boolean esValida(){
+        if(this.valor < 1000 && this.valor > 0 && this.tarjeta.esValida()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String tasa(){
+        return (tarjeta.marca + " " + tarjeta.tasa.valor(valor));
+    }
+
 }
